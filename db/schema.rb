7f45cfbc937bff0736718b34b3233624e41eb6de
thejennywang <thejennywang@gmail.com>
2014-08-12 10:47:55 +0100

    Revert "to be stashed"
    This reverts commit 4a3b4ac9ce766e01a23e32076c07617198a651f6.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811105926) do
+ActiveRecord::Schema.define(version: 20140811170208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,16 @@ ActiveRecord::Schema.define(version: 20140811105926) do
     t.datetime "updated_at"
     t.string   "category"
   end
+
+  create_table "reviews", force: true do |t|
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "restaurant_id"
+    t.integer  "rating"
+    t.string   "comment"
+  end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
 
 end
